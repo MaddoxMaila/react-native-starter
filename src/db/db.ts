028717d4f22bc261499f70961c5781307fbca91f
MaddoxMaila <maddoxmaila719@gmail.com>
2022-04-24 15:06:49 +0200

    Now we have a clear direction of where LiteORM implementation will go...

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,78 +2,78 @@ import {openDatabase, SQLiteDatabase, ResultSet, enablePromise} from 'react-nati
 
 enablePromise(true)
 
-export class LiteORM{
+export class LiteDb{
     sqlite!: SQLiteDatabase;
-    selectStatement: any
-    insertStatement: any
+    selectStatement: any = ""
+    insertStatement: any = ""
+    deleteStatement: any = ""
+
     
-    static db: LiteORM
+    static db: LiteDb
     
-    static  getInstance(): LiteORM{
+    static  getInstance(): LiteDb{
         if(!this.db){
-            this.db = new LiteORM()
+            this.db = new LiteDb()
             this.db.connect()
         }
         return this.db
     }
 
-    constructor(){
-        this.selectStatement = ""
-        this.insertStatement = ""
-    }
+    constructor(){}
     
     async connect(){
-        this.sqlite = await openDatabase({name: 'brackr.db', location: 'default'});
+        this.sqlite = await openDatabase({name: 'app.db', location: 'default'});
     }
+
 
     async getdb(){
         return this.sqlite
     }
 
     async createTable(tableName: String, columns: Array<String>){
-        const self = LiteORM.getInstance()
+        const self = LiteDb.getInstance()
         const query = `CREATE TABLE IF NOT EXISTS ${tableName}(${columns.toString()});`
         return await this.sqlite?.executeSql(query)
     }
 
     select(tableName: String){
-        const self = LiteORM.getInstance()
+        const self = LiteDb.getInstance()
         this.selectStatement = `SELECT * FROM ${tableName}`
         return this
     }
 
     where(condition: String){
-        const self = LiteORM.getInstance() 
+        const self = LiteDb.getInstance() 
         this.selectStatement = `${this.selectStatement} WHERE ${condition}`
         return this
     }
 
     limit(limit: number){
-        const self = LiteORM.getInstance() 
+        const self = LiteDb.getInstance() 
         this.selectStatement = `${this.selectStatement} LIMIT ${limit}`
         return this
     }
 
     ascending(){
-        const self = LiteORM.getInstance() 
+        const self = LiteDb.getInstance() 
         this.selectStatement = `${this.selectStatement} ASC`
         return this
     }
 
     descending(){
-        const self = LiteORM.getInstance() 
+        const self = LiteDb.getInstance() 
         this.selectStatement = `${this.selectStatement} DESC`
         return this
     }
 
     insert(tableName: String){
-        const self = LiteORM.getInstance() 
+        const self = LiteDb.getInstance() 
         self.insertStatement = `INSERT INTO ${tableName}`
         return self
     }
 
     async get(): Promise<ResultSet[]>{
-        const self = LiteORM.getInstance()
+        const self = LiteDb.getInstance()
         self.selectStatement = `${self.selectStatement};`
 
         return self.sqlite.executeSql(self.selectStatement)
@@ -81,10 +81,70 @@ export class LiteORM{
 
 
     async add(columns: Object): Promise<ResultSet[]>{
-        const self = LiteORM.getInstance()
+        const self = LiteDb.getInstance()
         this.insertStatement = `${this.insertStatement}(${Object.keys(columns).toString()}) VALUES(${Object.values(columns).toString()})`
 
         return this.sqlite.executeSql(this.insertStatement)
     }
 
+}
+
+export class LiteORM {
+    tableName: String = ""
+    columns: Array<String> = []
+    primaryKey: String = ""
+    db!: LiteDb
+    query: String = ""
+
+    constructor(){
+        this.db = LiteDb.getInstance()
+    }
+
+    getTableName(){
+        return this.tableName
+    }
+
+    getColumns(){
+        return this.columns
+    }
+    
+    async create(values: Object){
+        return await LiteDb.getInstance()
+                                        .insert(this.tableName)
+                                        .add(values)
+    }
+
+    find(){
+        this.query = `SELECT * FROM ${this.tableName}`
+        return this
+    }
+
+    where(where: String){
+        this.query = `${this.query} ${where}`
+        return this
+    }
+
+    async get(){
+        this.db.insertStatement = this.query
+        return await this.db.get()
+    }
+
+}
+
+export const connectToDb = async () => {
+    try{
+        await LiteDb.getInstance().connect()
+    }catch(e: any){
+        console.log("SQLite Connection Error")
+    }
+}
+
+export const createTables = async (tableModels: Array<any>) => {
+    try{
+        tableModels.forEach(async (model) => {
+            await LiteDb.getInstance().createTable(model?.getTableName(), model?.getColumns())
+        })
+    }catch(e){
+        console.log(`Table creation failed: ${e}`)
+    }
 }
